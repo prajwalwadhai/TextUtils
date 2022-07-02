1bf79253b4828cbaa861b610af92edd430216d20
prajwal wadhai <prajwalwadhai55@gmail.com>
2022-07-02 22:51:45 +0530

    video 6 is completed

---
+++ src/module2.mjs
@@ -0,0 +1,32 @@
+const a = "Harry";
+const b = "Rohan";
+const c = "Aakash";
+const d = "Priyanka";
+
+export default b;  
+export {a};  
+export {c};  
+export {d};  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
