fc269563b5aa4d22c037f5b3f846b4ca578419e4
prajwal wadhai <prajwalwadhai55@gmail.com>
2022-07-12 23:59:57 +0530

    Updates

---
+++ precache-manifest.04f6eaa0e94f32d129c548100ee98875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a4a3c7933d3b03eabd0",
+    "url": "/TextUtils/static/css/main.e805d4da.chunk.css"
+  },
+  {
+    "revision": "1a4a3c7933d3b03eabd0",
+    "url": "/TextUtils/static/js/main.1a4a3c79.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "7b559d997896220469de4b9a48d87d64",
+    "url": "/TextUtils/index.html"
+  }
+];
